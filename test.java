581fb0c7325cf02f314ee21d922a0011ba737ba3
Pietro Ferrara <pietro.ferrara@juliasoft.com>
2017-03-02 09:46:27 +0100

    eclipse project

---
+++ test.java
@@ -0,0 +1,27 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+public class test {
+
+    public static void main(String[] args) {
+        String string = null;
+        try {
+            PDFParser pdfParser = new PDFParser(new FileInputStream("sample.pdf"));
+            pdfParser.parse();
+            PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
+            PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
+            string = pdfTextStripper.getText(pdDocument);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        };
+        System.out.println(string);
+    }
+
+}
